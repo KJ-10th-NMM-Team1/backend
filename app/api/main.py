42c44c2f9f3b8b69430a0893b98d24319c89f46e
yunojang <hanganda23@naver.com>
2025-11-01 22:33:02 +0900

    fix: merge

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
 from .storage.routes import upload_router
-from app.router.editor_router import editor_router
+from .preview.router import preview_router
+from .project.project_router import router
+from .segment.router import segment_router
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(upload_router)
-api_router.include_router(editor_router, prefix="/editor", tags=["editor"])
+api_router.include_router(preview_router)
+api_router.include_router(router)
+api_router.include_router(segment_router)
