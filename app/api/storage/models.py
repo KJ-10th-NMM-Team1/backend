6b7f36ac41d09a6e3c830641f93d41124adceaae
yunojang <hanganda23@naver.com>
2025-11-01 15:58:37 +0900

    fe: project file upload apis

--- app/api/storage/models.py
+++ app/api/storage/models.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel
 class PresignRequest(BaseModel):
     filename: str
     content_type: str
+
+
+class UploadFinalize(BaseModel):
+    project_id: str
+    object_key: str
+
+
+class UploadFail(BaseModel):
+    project_id: str
--- app/api/storage/routes.py
+++ app/api/storage/routes.py
@@ -6,20 +6,24 @@ from uuid import uuid4
 from fastapi import APIRouter, HTTPException
 
 from app.config.s3 import s3
-from .models import PresignRequest
+from .models import PresignRequest, UploadFinalize
+from ..projects.modles import ProjectUpdate
+from ..deps import DbDep
+from app.api.projects.service import create_project, update_project
 
 upload_router = APIRouter(prefix="/storage", tags=["storage"])
 
 
-@upload_router.post(
-    "/presigned",
-)
-async def create_presigned_upload(payload: PresignRequest):
+@upload_router.post("/prepare-upload")
+async def prepare_upload(payload: PresignRequest, db: DbDep):
     bucket = os.getenv("AWS_S3_BUCKET")
     if not bucket:
         raise HTTPException(status_code=500, detail="AWS_S3_BUCKET env not set")
 
-    object_key = f"uploads/{datetime.now():%Y/%m/%d}/{uuid4()}_{payload.filename}"
+    project = await create_project(db, payload)
+    project_id = project["project_id"]
+
+    object_key = f"projects/{project_id}/inputs/videos/{uuid4()}_{payload.filename}"
 
     try:
         presigned = s3.generate_presigned_post(
@@ -35,7 +39,27 @@ async def create_presigned_upload(payload: PresignRequest):
         raise HTTPException(status_code=500, detail=f"presign 실패: {exc}")
 
     return {
+        "project_id": project_id,
         "upload_url": presigned["url"],
         "fields": presigned["fields"],
         "object_key": object_key,
     }
+
+
+@upload_router.post("/finish-upload")
+async def fin_upload(payload: UploadFinalize, db: DbDep):
+    update_payload = ProjectUpdate(
+        project_id=payload.project_id,
+        status="uploaded",
+        s3_key=payload.object_key,
+    )
+    result = await update_project(db, update_payload)
+    return {
+        "project_id": result["project_id"],
+        "status": "uploaded",
+    }
+
+
+# @upload_router.post("/fail-upload")
+# async def fail_upload(payload: PresignRequest, db: DbDep):
+#     pass
