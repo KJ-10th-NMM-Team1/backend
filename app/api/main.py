e637b6528fb5bbd0a72e4b187efa1263b9792fa9
anjun206 <malssungjiwon@gmail.com>
2025-11-01 16:05:07 +0900

    ai_dubbing_seg_preview_backend

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .storage.routes import upload_router
+from app.router.editor_router import editor_router
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(upload_router)
+api_router.include_router(editor_router, prefix="/editor", tags=["editor"])
