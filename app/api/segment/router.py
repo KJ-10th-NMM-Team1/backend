7e3821029e2fdaf0d0286f0846baf1a7e69a3f64
kimhyunso <87798982+kimhyunso@users.noreply.github.com>
2025-11-03 21:09:31 +0900

    fix: 세그먼트 service, router 충돌해결 (#48)
    * feat: stage
    
    * t
    
    * feat: 환경설정 변경
    
    * feat: README에 .env 설정 규칙 추가 env_config 파일 이름변경
    
    * feat: segment api 수정
    
    * fix: main env, CORS 충돌 해결
    
    * feat: 세그먼트 update API 추가
    
    * feat: 로그인 기능 구현
    
    * feat: 회원가입 기능 구현
    
    * conflict: 세그먼트 api 충돌 해결
    
    * fix: 세그먼트 service 삭제
    
    * Delete app/api/segment/service.py
    
    중복
    
    ---------
    
    Co-authored-by: 진주영 <67181810+jjy3385@users.noreply.github.com>

--- app/api/segment/router.py
+++ app/api/segment/router.py
@@ -2,18 +2,34 @@ from datetime import datetime
 from uuid import uuid4
 from fastapi import APIRouter, HTTPException
 from fastapi import Depends
-from .service import SegmentService
-from .model import ResponseSegment
+from .segment_service import SegmentService
+from .history_service import HistoryService
+from .model import ResponseSegment, RequestSegment
 from typing import List
 
 segment_router = APIRouter(prefix="/segment", tags=["segment"])
 
-@segment_router.get('/', response_model=List[ResponseSegment])
-async def get_segment_all(service: SegmentService = Depends(SegmentService)):
-    find_list = await service.find_all_segment()
+
+@segment_router.get("/{project_id}", response_model=List[ResponseSegment])
+async def get_segment_all(
+    project_id: str, service: SegmentService = Depends(SegmentService)
+):
+    find_list = await service.find_all_segment(project_id)
     return find_list
 
 
+@segment_router.put("/{project_id}/history", response_model=str)
+async def segment_history(
+    request: RequestSegment, history_service: HistoryService = Depends(HistoryService)
+):
+    try:
+        return await history_service.insert_one_history(request)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-
+@segment_router.patch("/{project_id}/save")
+async def save_segment(
+    request: RequestSegment, service: SegmentService = Depends(SegmentService)
+):
+    return await service.update_segment(request)
