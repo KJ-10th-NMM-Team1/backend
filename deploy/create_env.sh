d2a30d02e5f92658ed66e6a639eab0de70764efc
kimhyunso <gustn9515@gmail.com>
2025-11-13 15:45:46 +0900

    fix: 배포 스크립트 youtube 쿠키 추가

--- deploy/create_env.sh
+++ deploy/create_env.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
-# 1. AWS Secrets Manager에서 .env 내용을 가져옵니다.
-#    (EC2의 IAM 역할이 이 Secret에 대한 GetSecretValue 권한을 가지고 있어야 합니다.)
-#    (EC2가 사용하는 리전으로 'ap-northeast-2'를 수정하세요)
-#    ("my-app/env" 부분을 Secrets Manager에 저장한 실제 보안 암호 이름으로 변경하세요)
+APP_DIR="/home/ubuntu/app"
+ENV_FILE="$APP_DIR/.env"
+COOKIE_FILE="$APP_DIR/youtube_cookies.txt" # <-- 1. 쿠키 파일 경로 정의
+
+echo "Creating .env file..."
 aws secretsmanager get-secret-value \
     --secret-id "aws-dupliot-app/env" \
     --region ap-northeast-2 \
     --query SecretString \
-    --output text | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' > /home/ubuntu/app/.env
+    --output text | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' > $ENV_FILE
 
-SA_JSON=$(grep '^GCP_SERVICE_ACCOUNT_JSON=' /home/ubuntu/app/.env | cut -d= -f2-)
-printf '%s\n' "$SA_JSON" > /home/ubuntu/app/gcp-sa.json
+SA_JSON=$(grep '^GCP_SERVICE_ACCOUNT_JSON='$ENV_FILE | cut -d= -f2-)
+YOUTUBE_COOKIE_FILE=$(grep '^YOUTUBE_COOKIE='$ENV_FILE | cut -d= -f2-)
+
+printf '%s\n' "$SA_JSON" > $APP_DIR/gcp-sa.json
+printf '%s\n' "$YOUTUBE_COOKIE_FILE" > $APP_DIR/youtube_cookie.txt
+
 chmod 600 /home/ubuntu/app/gcp-sa.json
+chmod 600 /home/ubuntu/app/youtube_cookie.txt
 
-# 2. 생성된 .env 파일의 소유자를 'ubuntu' 사용자로 변경합니다.
-#    (appspec.yml의 destination 경로와 runas 사용자와 일치시킵니다.)
 chown ubuntu:ubuntu /home/ubuntu/app/.env
 chmod 600 /home/ubuntu/app/.env # (보안을 위해 소유자만 읽고 쓸 수 있도록 설정)
-
 
 
 echo ".env file created successfully."
