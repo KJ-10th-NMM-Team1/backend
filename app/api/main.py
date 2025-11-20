17b83e821868c7d81da61dcd905793e580b143bf
yunojang <54504352+yunojang@users.noreply.github.com>
2025-11-20 22:40:38 +0900

    events endpoint (#177)
    * fe: progress events dispatch
    
    * fix: remove comment

--- app/api/main.py
+++ app/api/main.py
@@ -21,6 +21,7 @@ from .audio.router import audio_router
 from .mux.router import router as mux_router
 from .progress.router import progress_router
 from .accent.router import router as accent_router
+from .progress.router import progress_router
 
 api_router = APIRouter(prefix="/api")
 
@@ -48,3 +49,4 @@ api_router.include_router(audio_router)
 api_router.include_router(mux_router)
 api_router.include_router(progress_router)
 api_router.include_router(accent_router)
+api_router.include_router(progress_router)
